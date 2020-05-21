33afac51f74b7e43ae6252b34f406d588a2e1f3a
Riley Thompson <jr3thomp@edu.uwaterloo.ca>
2020-05-20 21:05:29 -0400

    fill multiple shapes diff colors

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -2,25 +2,76 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.time.Instant;
 
 public class FloodFill {
-    private final int len = 256;
-    private final int outlineLen = 128;
+    private final int len = 360;
     private final int IMG_TYPE = 5;
     private int[][] board;
     private int curx;
     private int cury;
+    private int outlineLen;
     private int fills;
 
     public FloodFill() throws IOException {
         fills = 0;
         board = new int[len][len];
         System.out.println("Running FloodFill...");
-        long start = Instant.now().toEpochMilli();
 
-        cury = len/4;
-        curx = len/2;
+        // start draw outline
+
+        drawOutline(len/16, len/2, len/2);
+        drawOutline(len/16 + len/8, len/2, len/4);
+
+
+
+        drawOutline(len/16 + 175, len/4 + 10, len/4 + 20);
+        drawOutline(len/16 + len/16 + 185, len/4 + 10, len/8);
+
+
+
+        drawOutline(len/16 + 175, len/2 + len/4 - 10, len/4 + 20);
+        drawOutline(len/16 + len/16 + 185, len/2 + len/4 - 10, len/8);
+
+        // end draw outline
+
+        genImage("outline");
+
+        try {
+            fill(len/8, len/2, 2);
+        }
+        catch (StackOverflowError e) {
+            System.out.println("Caught stack overflow error after " + fills + " calls");
+            genImage("floodfill");
+            return;
+        }
+
+        try {
+            fill(213, len/4 + 10, 3);
+        }
+        catch (StackOverflowError e) {
+            System.out.println("Caught stack overflow error after " + fills + " calls");
+            genImage("floodfill");
+            return;
+        }
+
+        try {
+            fill(213, len/2 + len/4 - 10, 4);
+        }
+        catch (StackOverflowError e) {
+            System.out.println("Caught stack overflow error after " + fills + " calls");
+            genImage("floodfill");
+            return;
+        }
+
+        genImage("floodfill");
+    }
+
+    private void drawOutline(int inity, int initx, int initlen) {
+//        cury = len/4;
+//        curx = len/2;
+        cury = inity;
+        curx = initx;
+        outlineLen = initlen;
 
         // down and left
         updateBoard(1, -1);
@@ -33,7 +84,7 @@ public class FloodFill {
 
         // up and left
         cury--;
-        while ((curx >= len/2 && cury >= len/4) && (board[cury][curx] != 1)) {
+        while ((curx >= initx && cury >= inity) && (board[cury][curx] != 1)) {
             board[cury][curx] = 1;
             double dir = Math.random()*2;
 
@@ -48,61 +99,49 @@ public class FloodFill {
 
         }
 
-        if (cury + 1 == len/4 || cury == len/4) {
-            if (cury + 1 == len/4) {
+        if (cury + 1 == inity || cury == inity) {
+            if (cury + 1 == inity) {
                 cury++;
             }
 
-            for (int x = curx; x >= len/2; --x) {
+            for (int x = curx; x >= initx; --x) {
                 board[cury][x] = 1;
             }
         }
 
-        if (curx + 1 == len/2 || curx == len/2) {
-            if (curx + 1 == len/2) {
+        if (curx + 1 == initx || curx == initx) {
+            if (curx + 1 == initx) {
                 curx++;
             }
 
-            if (cury <= len/4) {
-                for (int y = cury; y <= len/4; ++y) {
+            if (cury <= inity) {
+                for (int y = cury; y <= inity; ++y) {
                     board[y][curx] = 1;
                 }
             }
             else {
-                for (int y = cury; y >= len/4; --y) {
+                for (int y = cury; y >= inity; --y) {
                     board[y][curx] = 1;
                 }
             }
         }
-
-        genImage("outline");
-
-        try {
-            fill(len/4 + outlineLen/2, len/2);
-        }
-        catch (StackOverflowError e) {
-            System.out.println("Caught stack overflow error after " + fills + " calls");
-            genImage("floodfill");
-        }
-
-        genImage("floodfill");
     }
 
-    private void fill(int y, int x) throws StackOverflowError {
+    private void fill(int y, int x, int colorCode) throws StackOverflowError {
         fills++;
-        board[y][x] = 2;
+        board[y][x] = colorCode;
 
         if (board[y - 1][x] == 0) {
-            fill(y - 1, x);
+            fill(y - 1, x, colorCode);
         }
         if (board[y + 1][x] == 0) {
-            fill(y + 1, x);
+            fill(y + 1, x, colorCode);
         }
         if (board[y][x - 1] == 0) {
-            fill(y, x - 1);
+            fill(y, x - 1, colorCode);
         }
         if (board[y][x + 1] == 0) {
-            fill(y, x + 1);
+            fill(y, x + 1, colorCode);
         }
     }
 
@@ -122,6 +161,18 @@ public class FloodFill {
                     int rgb = 64;
                     rgb = (rgb << 8) + 128;
                     rgb = (rgb << 8) + 255;
+                    img.setRGB(x, y, rgb);
+                }
+                else if (board[y][x] == 3) {
+                    int rgb = 255;
+                    rgb = (rgb << 8) + 128;
+                    rgb = (rgb << 8) + 64;
+                    img.setRGB(x, y, rgb);
+                }
+                else if (board[y][x] == 4) {
+                    int rgb = 64;
+                    rgb = (rgb << 8) + 255;
+                    rgb = (rgb << 8) + 128;
                     img.setRGB(x, y, rgb);
                 }
                 else {
